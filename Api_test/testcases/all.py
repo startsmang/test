aba9e99bcab9eb26c5a389d6d6dc82986dbcc805
liao <10331399+liaoshengqi@user.noreply.gitee.com>
2023-02-23 16:19:36 +0800

    11

--- Api_test/testcases/all.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import os
-
-
-if __name__ == '__main__':
-    # 执行pytest单元测试，生成 Allure 报告需要的数据存在 /temp 目录
-    pytest.main(['-vs','--alluredir' , './temp'])
-    # 执行命令 allure generate ./temp -o ./report --clean ，生成测试报告
-    os.system('allure generate ./temp -o ./report --clean')
